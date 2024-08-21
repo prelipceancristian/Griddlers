e48f665d6e817524ee980c0965e30b3964d640c1
Cristian Prelipcean <prelipceancristian100@gmail.com>
2024-08-21 13:24:14 +0300

    Missed file

--- fe/src/models/Grid.ts
+++ fe/src/models/Grid.ts
@@ -1,13 +1,21 @@
 class Grid {
-  Id!: Number;
+  static buildGrid = (
+    id: number,
+    authorId: number,
+    gridContent: string,
+  ): Grid => {
+    const grid = new Grid();
+    grid.Id = id;
+    grid.AuthorId = authorId;
+    grid.GridContent = gridContent;
+    return grid;
+  };
 
-  AuthorId!: Number;
-
+  Id!: number;
+  AuthorId!: number;
   GridContent!: string;
 
-  // dunno why this is static
   static parseGridContent = (content: string): boolean[][] => {
-    console.log("here");
     const numberMatrix: Number[][] = JSON.parse(content);
     const booleanMatrix: boolean[][] = numberMatrix.map((innerArray) =>
       innerArray.map((num) => !!num),
