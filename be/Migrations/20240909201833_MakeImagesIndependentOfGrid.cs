616c3f7fe8c24e67bfdc9b8c5ae8dd5694a6d11b
Cristian Prelipcean <prelipceancristian100@gmail.com>
2024-09-19 18:49:06 +0300

    Missed migrations

---
+++ be/Migrations/20240909201833_MakeImagesIndependentOfGrid.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Griddlers.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeImagesIndependentOfGrid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Grids_GridId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_GridId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "GridId",
+                table: "Images");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageId",
+                table: "Grids",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grids_ImageId",
+                table: "Grids",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grids_Images_ImageId",
+                table: "Grids",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "ImageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grids_Images_ImageId",
+                table: "Grids");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Grids_ImageId",
+                table: "Grids");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Grids");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GridId",
+                table: "Images",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_GridId",
+                table: "Images",
+                column: "GridId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Grids_GridId",
+                table: "Images",
+                column: "GridId",
+                principalTable: "Grids",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
