f8153c38fe0ef4dd83ee4bff1daa8bff1d50f1ba
Cristian Prelipcean <prelipceancristian100@gmail.com>
2025-02-23 00:08:10 +0200

    Add a (temporary?) logo

--- fe/src/components/main/Layout.tsx
+++ fe/src/components/main/Layout.tsx
@@ -4,7 +4,9 @@ import "./Layout.css";
 function Layout(): JSX.Element {
   return (
     <div className="HomeContainer">
-      <div className="HomeHeader"> Griddlers </div>
+      <div className="HomeHeader">
+        <img src="logo.png" alt="logo" height={55} /> {"  "} Griddlers
+      </div>
       <div className="GridContainer">
         <Outlet></Outlet>
       </div>
